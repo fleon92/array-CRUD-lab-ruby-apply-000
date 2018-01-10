059e8913ba02ce619ff71317f4a96fe00f4279b6
Felipe Leon <felipeleon92@gmail.com>
2018-01-10 19:44:33 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["hey","you","arrays"]
+  array = ["hey","you","arrays!"]
   array.pop
 end
 
